321abe3889117b3dd3384e108fb6dab9df29cc48
Fernando Schlottmann <fgc.schlottmann@gmail.com>
2021-04-11 22:19:05 -0300

    clase 7 menu con bootstrap.js

--- public/js/app.js
+++ public/js/app.js
@@ -2,14 +2,32 @@
   var app = angular.module('pokedex', [])
   
   app.controller('PokemonController', function() {
+
+    this.tab = 1;
+
     this.pokemon = {
-      id: 001,
+      id: "001",
       name: 'Bulbasaur',
-      species: 'Seed Pokemon',
+      species: 'Seed Pokémon',
       type: ['Grass', 'Poison'],
-      height: "2'4",
-      weight: '15.2 lbs',
-      abilities: ['Overgrow', 'Chlorophyll']
+      height: "2'4 (0.71m)",
+      weight: '15.2 lbs (6.9kg)',
+      abilities: ['Overgrow', 'Chlorophyll'],
+      stats: {
+        hp: 45,
+        attack: 49,
+        defense: 49,
+        'sp.atk': 65,
+        'sp.def': 65,
+        speed: 45,
+        total: 318
+      },
+      evolution: ['Bulbasaur', 'Ivysaur', 'Venusaur']
     }
+
+    this.selectTab = function(tab) {
+      this.tab = tab
+    }
+
   })
 })()
