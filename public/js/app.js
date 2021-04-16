6ab15f1f159a9a87fdaf9ac266406b1a7b8a7754
Fernando Schlottmann <fgc.schlottmann@gmail.com>
2021-04-16 02:58:48 -0300

    created others elments directives and his partials

--- public/js/app.js
+++ public/js/app.js
@@ -60,11 +60,39 @@
     }
   })
 
-  app.directive('pokemonData', function(){
+  app.directive('pokemonName', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'partials/pokemon-name.html'
+    }
+  })
+
+  app.directive('pokemonImage', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'partials/pokemon-image.html'
+    }
+  })
+
+  app.directive('pokemonData', function() {
     return {
       restrict: 'E',
       templateUrl: 'partials/pokemon-data.html'
     }
   })
+
+  app.directive('pokemonStats', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'partials/pokemon-stats.html'
+    }
+  })
   
+  app.directive('pokemonEvolution', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'partials/pokemon-evolution.html'
+    }
+  })
+
 })()
