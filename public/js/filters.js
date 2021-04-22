64df90724c34758e613d0e09d462672f5a6b12b1
Ferchax <fgc.schlottmann@gmail.com>
2021-04-22 00:15:30 -0300

    Add filter to normalize images

--- public/js/filters.js
+++ public/js/filters.js
@@ -1,9 +1,18 @@
 (() => {
     angular.module('pokedex.filters', [])
-    .filter('imageify', function () {
+    .filter('normalize', function () {
         return function (input) {
-          var url = "img/pokemons/" + input.toLowerCase() + ".jpg"
-          return url
+          input = input
+                  .replace('♀', 'f')
+                  .replace('♂', 'm')
+                  .replace(/\W+/g, "")
+          return input.toLowerCase()
         }
     })
+    .filter('imageify', ['$filter', function($filter){
+      return function(input){
+        var url = "img/pokemons/" + $filter('normalize')(input) + ".jpg"
+        return url
+      }
+    }])
 })()
