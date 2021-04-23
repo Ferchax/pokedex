ad001e250a8c2b131c81afda025f7e3c864c58be
Ferchax <fgc.schlottmann@gmail.com>
2021-04-23 12:55:12 -0300

    Add by name function to service

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,42 +1,31 @@
 (function() {    
-    angular.module('pokedex.controllers', [])
-      .controller('PokedexController', ['$scope', 'pokemonService', function($scope, pokemonService) {
 
-        pokemonService.all().then(function(data){
-          $scope.pokemons = data;
-        });
-        
-      }])  
-      .controller('PokemonController', ['$scope', function ($scope) {
+  angular.module('pokedex.controllers', [])
+  
+  .controller('PokedexController', ['$scope', 'pokemonService', function($scope, pokemonService) {
 
-          $scope.pokemon = {
-            id: "001",
-            name: 'Bulbasaur',
-            species: 'Seed Pokémon',
-            type: ['Grass', 'Poison'],
-            height: "2'4 (0.71m)",
-            weight: '15.2 lbs (6.9kg)',
-            abilities: ['Overgrow', 'Chlorophyll'],
-            stats: {
-              hp: 45,
-              attack: 49,
-              defense: 49,
-              'sp.atk': 65,
-              'sp.def': 65,
-              speed: 45,
-              total: 318
-            },
-            evolution: ['Bulbasaur', 'Ivysaur', 'Venusaur']
-          };
+    pokemonService.all().then(function(data){
+      $scope.pokemons = data;
+    });
+    
+  }])  
+  .controller('PokemonController', ['$scope', 'pokemonService', function ($scope, pokemonService) {
 
-      }])
-      .controller('TabsController', function() {
-
-          this.tab = 1
+      $scope.pokemon = {};
       
-          this.selectTab = function(tab) {
-            this.tab = tab
-          }
-
+      pokemonService.byName('bulbasaur')
+      .then(function(data) {
+        $scope.pokemon = data;
       });
+
+  }])
+  .controller('TabsController', function() {
+
+      this.tab = 1;
+  
+      this.selectTab = function(tab) {
+        this.tab = tab;
+      };
+
+  });
 })();
--- public/js/services.js
+++ public/js/services.js
@@ -2,8 +2,10 @@
 
     angular.module('pokedex.services', [])
         
-    .factory('pokemonService', ['$http', '$q', function($http, $q) {
-        
+    .factory('pokemonService', ['$http', '$q', '$filter', function($http, $q, $filter) {
+    
+        var normalize = $filter('normalize');
+
         function all() {
             var deferred = $q.defer();
 
@@ -15,7 +17,29 @@
             return deferred.promise;
         }
 
-        return { all: all }
+        function byName(name) {
+            name = normalize(name);
+            var deferred = $q.defer();
+
+            all().then(function(data){
+                var results = data.filter(function(pokemon){
+                    return normalize(pokemon.name) === name;
+                });
+
+                if(results.length > 0) {
+                    deferred.resolve(results[0]);
+                } else {
+                    deferred.reject();
+                }
+            });
+
+            return deferred.promise;
+        }
+
+        return { 
+            all: all,
+            byName: byName
+        };
     }]);
 
 })();
