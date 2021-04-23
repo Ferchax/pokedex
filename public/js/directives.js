9c35359b16ded4adabe38c69802ec249ec7d137f
Ferchax <fgc.schlottmann@gmail.com>
2021-04-23 00:08:27 -0300

    Create pokemon type directive

--- public/js/directives.js
+++ public/js/directives.js
@@ -58,4 +58,10 @@
           controllerAs: 'cmtsCtrl'
         }
     })
+    .directive('pokemonType', function(){
+      return {
+        restrict: 'E',
+        templateUrl: 'partials/pokemon-type.html'
+      }
+    })
 })()
